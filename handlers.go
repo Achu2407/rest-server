6bd250790e435255971ca83c8d87ce742cf340e5
Achanandhi-M <achanandhi.m@gmail.com>
2025-05-18 15:42:10 +0530

    test: keploy utg-staging

--- handlers.go
+++ handlers.go
@@ -22,8 +22,10 @@ type Server struct {
 	CPUProfile           string
 	TLSKey               string
 	TLSCert              string
+	TLSMinVer            string
 	TLS                  bool
 	NoAuth               bool
+	ProxyAuthUsername    string
 	AppendOnly           bool
 	PrivateRepos         bool
 	Prometheus           bool
